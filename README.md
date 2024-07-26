7e7ccfb027e895cd2754fd2d0d737a09c3db8962
Adrian <adrianhartanto0312@gmail.com>
2024-07-26 12:01:41 +0700

    Feat: Change error handling and editor run

---
+++ README.md
@@ -0,0 +1,3 @@
+# Script-Pad
+
+This project is a text editor that I made following a tutorial by Philipp Flenker titled [hecto: Build Your Own Text Editor in Rust](https://flenker.blog/hecto/).
--- src/editor.rs
+++ src/editor.rs
@@ -1,34 +1,52 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
 
-pub struct Editor {}
+pub struct Editor {
+    should_exit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Self {}
+        Self { should_exit: false }
     }
 
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        for key_press in io::stdin().bytes() {
-            match key_press {
-                Ok(key_press) => {
-                    let char: char = key_press as char;
-                    if char.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", key_press);
-                    } else {
-                        println!(
-                            "Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",
-                            key_press, char
-                        );
-                    }
-                    if char == 'q' {
-                        break;
-                    }
+    // pub fn draw_rows(&self) {
+    //     if let Ok((col, row)) = crossterm::terminal::size()? {}
+    // }
+
+    pub fn run(&mut self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye! :D");
+    }
+
+    pub fn repl(&mut self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        // self.draw_rows();
+        loop {
+            if let Key(KeyEvent {
+                code,
+                modifiers,
+                kind,
+                state,
+            }) = read()?
+            {
+                print!(
+                    "Code: {code:?}, Modifiers: {modifiers:?}, Kind: {kind:?}, State: {state:?}\n"
+                );
+
+                match code {
+                    Char('q') => self.should_exit = true,
+                    _ => (),
                 }
-                Err(err) => println!("Error: {:?}", err),
+            }
+
+            if self.should_exit {
+                break;
             }
         }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
