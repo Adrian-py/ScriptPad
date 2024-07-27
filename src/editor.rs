0892a269ec626a93b57bfc4ac67efd18c50d452f
Adrian <adrianhartanto0312@gmail.com>
2024-07-28 02:38:57 +0700

    Feat: add tildes on the left side of each row

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,10 @@
+use std::io::Write;
+
+use crossterm::cursor::MoveTo;
 use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::queue;
+use crossterm::style::{Color, PrintStyledContent, Stylize};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 
 pub struct Editor {
     should_exit: bool,
@@ -10,9 +15,22 @@ impl Editor {
         Self { should_exit: false }
     }
 
-    // pub fn draw_rows(&self) {
-    //     if let Ok((col, row)) = crossterm::terminal::size()? {}
-    // }
+    pub fn draw_rows(&self) -> std::io::Result<()> {
+        match crossterm::terminal::size() {
+            Ok((col, _)) => {
+                let tilde = "~ ".with(Color::Green);
+                for i in 0..col {
+                    queue!(std::io::stdout(), MoveTo(0, i), PrintStyledContent(tilde))?;
+                }
+                queue!(std::io::stdout(), MoveTo(2, 0))?;
+                std::io::stdout().flush()?;
+            }
+            Err(error) => {
+                println!("error: {error:?}");
+            }
+        }
+        Ok(())
+    }
 
     pub fn run(&mut self) {
         if let Err(err) = self.repl() {
@@ -23,7 +41,10 @@ impl Editor {
 
     pub fn repl(&mut self) -> Result<(), std::io::Error> {
         enable_raw_mode()?;
-        // self.draw_rows();
+        print!("\x1b[2J");
+        if let Err(error) = self.draw_rows() {
+            panic!("error: {error:?}");
+        }
         loop {
             if let Key(KeyEvent {
                 code,
