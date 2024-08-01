94249de5e19cc7f4e8e43bf6de2deda8763e8f0f
Adrian <adrianhartanto0312@gmail.com>
2024-08-01 21:07:39 +0700

    Feat: Add terminal manipulation capabilities

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,6 @@
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use std::io::{stdout, Error, Write};
-use terminal::Terminal;
+use std::io::Error;
+use terminal::{CursorPosition, Terminal, TerminalSize};
 mod terminal;
 
 pub struct Editor {
@@ -28,32 +28,40 @@ impl Editor {
             }
 
             let event = read()?;
-            self.handle_event(event);
+            self.handle_event(event)?;
         }
         Ok(())
     }
 
     fn refresh_screen(&mut self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
+        Terminal::move_cursor_to(CursorPosition { x: 0, y: 0 })?;
         if self.should_exit {
             Terminal::clear_screen()?;
-            println!("Goodbye! :D");
+            Terminal::print("Goodbye! :D")?;
         } else {
             Self::draw_row_borders()?;
+            Terminal::move_cursor_to(CursorPosition { x: 2, y: 0 })?;
         }
-
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
     fn draw_row_borders() -> Result<(), Error> {
-        let (_width, height) = Terminal::get_size()?;
-        for i in 0..=height {
-            Terminal::move_cursor_to(0, i)?;
-            stdout().write(b"~ ")?;
+        let terminal_size: TerminalSize = Terminal::get_size()?;
+        for curr_row in 0..terminal_size.height {
+            Terminal::clear_line()?;
+            Terminal::print("~ ")?;
+
+            if curr_row + 1 < terminal_size.height {
+                Terminal::print("\r\n")?;
+            }
         }
         Ok(())
     }
 
-    fn handle_event(&mut self, event: Event) {
+    fn handle_event(&mut self, event: Event) -> Result<(), Error> {
         // If CTRL + q is pressed, then terminate
         if let Key(KeyEvent {
             code: Char('q'),
@@ -62,14 +70,15 @@ impl Editor {
         }) = event
         {
             self.should_exit = true;
-            return;
+            return Ok(());
         }
 
         if let Key(KeyEvent {
             code: Char(key), ..
         }) = event
         {
-            println!("Pressed key: {key}");
+            Terminal::print(format!("Pressed: {}", key).as_str())?;
         }
+        Ok(())
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,37 +1,77 @@
-use std::io::stdout;
-
 use crossterm::{
-    cursor::MoveTo,
-    execute,
+    cursor::{Hide, MoveTo, Show},
+    queue,
+    style::Print,
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
+use std::io::{stdout, Error, Write};
+
+#[derive(Copy, Clone)]
+pub struct CursorPosition {
+    pub x: u16,
+    pub y: u16,
+}
+
+#[derive(Copy, Clone)]
+pub struct TerminalSize {
+    pub width: u16,
+    pub height: u16,
+}
 
 pub struct Terminal {}
 
 impl Terminal {
-    pub fn initialize() -> Result<(), std::io::Error> {
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(CursorPosition { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
 
-    pub fn terminate() -> Result<(), std::io::Error> {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
 
-    pub fn clear_screen() -> Result<(), std::io::Error> {
-        execute!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn clear_line() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn get_size() -> Result<(u16, u16), std::io::Error> {
-        crossterm::terminal::size()
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
+    pub fn get_size() -> Result<TerminalSize, Error> {
+        let (width, height) = crossterm::terminal::size()?;
+        Ok(TerminalSize { width, height })
+    }
+
+    pub fn print(str: &str) -> Result<(), Error> {
+        queue!(stdout(), Print(str))?;
+        Ok(())
+    }
+
+    pub fn move_cursor_to(pos: CursorPosition) -> Result<(), Error> {
+        queue!(stdout(), MoveTo(pos.x, pos.y))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        queue!(stdout(), Show)?;
+        Ok(())
     }
 }
